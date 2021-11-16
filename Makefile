1e580be10978f10cfb9db7c71edb27a5263253ab
alaamimi <aku-no-hana@outlook.fr>
2021-11-16 17:22:38 +0100

    INIT

--- Makefile
+++ Makefile
@@ -5,32 +5,9 @@
 #                                                     +:+ +:+         +:+      #
 #    By: ablaamim <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2021/11/14 22:08:19 by ablaamim          #+#    #+#              #
-#    Updated: 2021/11/16 11:38:12 by ablaamim         ###   ########.fr        #
+#    Created: 2021/11/16 17:22:23 by ablaamim          #+#    #+#              #
+#    Updated: 2021/11/16 17:22:25 by ablaamim         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-NAME = libftprintf.a
 
-CC = gcc
-CFLAGS = -Wall -Wextra -Werror
-
-SRCS = ft_printf.c \
-	utils.c
-
-OBJS = $(SRCS:%.c=%.o)
-
-all : $(NAME)
-
-$(NAME): $(OBJS)
-	ar -rcs $(NAME) $(OBJS)
-
-clean :
-	rm -rf $(OBJS)
-
-fclean : clean
-	rm -rf $(NAME)
-
-re : fclean all
-
-.PHONY : all clean fclean re
--- ft_printf.c
+++ ft_printf.c
@@ -1,22 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ablaamim <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/16 17:20:18 by ablaamim          #+#    #+#             */
-/*   Updated: 2021/11/16 17:22:17 by ablaamim         ###   ########.fr       */
+/*   Created: 2021/11/16 17:19:52 by ablaamim          #+#    #+#             */
+/*   Updated: 2021/11/16 17:19:57 by ablaamim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-#define FT_PRINTF_H
 
-# include <stdarg.h>
-# include <unistd.h>
-# include <stdlib.h>
-
-int	ft_printf(const char *format, ...);
-
-#endif
