f04b233bf3f84f467478ab8d5421400d80a39913
alaamimi <aku-no-hana@outlook.fr>
2022-01-01 19:47:15 +0100

    Project structure

---
+++ includes/libftprintf.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ablaamim <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/01 19:41:18 by ablaamim          #+#    #+#             */
+/*   Updated: 2022/01/01 19:46:39 by ablaamim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+# include "../libft/libft.h"
+#endif
