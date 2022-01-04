0d8531143a5ddf668c9adbf32ac2ed702404d4c3
alaamimi <aku-no-hana@outlook.fr>
2022-01-05 00:47:04 +0100

    PARSING_FLAGS DONE

--- srcs/ft_parsing.c
+++ srcs/ft_parsing.c
@@ -6,7 +6,7 @@
 /*   By: ablaamim <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/04 17:31:57 by ablaamim          #+#    #+#             */
-/*   Updated: 2022/01/04 17:48:09 by ablaamim         ###   ########.fr       */
+/*   Updated: 2022/01/05 00:37:42 by ablaamim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,23 @@
 
 void	ft_flags_parser(t_fmt *fmt, t_holder *holder)
 {
-
+	char	*tmp;
+	if(!holder->prefix)
+		holder->prefix = ft_strdup("");
+	while (ft_strchr(HOLDER_ALL_FLAGS, fmt->format[fmt->i]))
+	{
+		if (fmt->format[fmt->i] == HOLDER_JUSTIFY)
+			holder->left_justify = 1;
+		if (ft_strchr(HOLDER_PREFIX, fmt->format[fmt->i]))
+		{
+			tmp = holder->prefix;
+			holder->prefix = ft_append_char(tmp, fmt->format[fmt->i]);
+			free(tmp);
+		}
+		if (fmt->format[fmt->i] == HOLDER_PADDING)
+			holder->padding = HOLDER_PADDING;
+		fmt->i++;
+	}
 }
 
 void	*ft_parsing(t_fmt *fmt, t_holder *holder)
